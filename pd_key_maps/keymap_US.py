6e00a4c3c095bd3d2f0dd81d10cf2e648a4b6111
gitdev-bash <63194733+gitdev-bash@users.noreply.github.com>
2021-06-01 17:16:52 +0200

    Separate Key maps from the main program + gitignore

---
+++ pd_key_maps/keymap_US.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+commap = {}
+c1map = {
+    "a": 4,
+    "b": 5,
+    "c": 6,
+    "d": 7,
+    "e": 8,
+    "f": 9,
+    "g": 10,
+    "h": 11,
+    "i": 12,
+    "j": 13,
+    "k": 14,
+    "l": 15,
+    "m": 16,
+    "n": 17,
+    "o": 18,
+    "p": 19,
+    "q": 20,
+    "r": 21,
+    "s": 22,
+    "t": 23,
+    "u": 24,
+    "v": 25,
+    "w": 26,
+    "x": 27,
+    "y": 28,
+    "z": 29,
+    "1": 30,
+    "2": 31,
+    "3": 32,
+    "4": 33,
+    "5": 34,
+    "6": 35,
+    "7": 36,
+    "8": 37,
+    "9": 38,
+    "0": 39,
+    "RETURN": 40,
+    "ESC": 41,
+    "BACKSPACE": 42,
+    "TAB": 43,
+    "SPACE": 44,
+}
+c2map = {}
+for i in range(65, 91):
+    c2map[chr(i)] = "SHIFT " + chr(i).lower()
+aliasmap = {
+    "CTRL-ALT": "CTRL ALT",
+    "CTRL-SHIFT": "CTRL SHIFT",
+    "DEFAULT_DELAY": "DEFAULTDELAY",
+}
